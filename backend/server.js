95da2548c03b99a57aa83d1a9354859c92303f46
Rodrigo Piccilli <rgpiccil@ncsu.edu>
2025-05-23 17:57:15 -0400

    Set Up API, modified Port.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
-const PORT = 5000;
-
 const server = express();
+const PORT = 5050;
+
+const CPU = require('./src/CPU.js');
+
+
+server.get("/cpu", (req, res) => {
+
+    const cpuMove = CPU.getCPUMove();
+
+    res.json({cpuMove : cpuMove})
+
+})
+
+
+
+
 server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
